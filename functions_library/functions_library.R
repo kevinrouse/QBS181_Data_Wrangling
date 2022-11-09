88bf1f3f023d4daaf55377d41f340db4e49a24e3
anton-m-hung <84089123+anton-m-hung@users.noreply.github.com>
2022-11-08 23:01:03 -0500

    finalized putting weather plotting code into functions library script

--- functions_library/functions_library.R
+++ functions_library/functions_library.R
@@ -56,3 +56,69 @@ assess_normality <- function(dataset){
   boxplot(dataset$Actual.difference...spread)
   qqPlot(dataset$Actual.difference...spread)
 }
+
+
+############## Missing value markdown
+plot_playing_surface <- function(dataset){
+  surface_data <- filter(dataset, 
+                         stadium_surface=='FieldTurf' |
+                           stadium_surface=='Hellas Matrix Turf' |
+                           stadium_surface=='Grass')
+  
+  ggplot(data=surface_data, aes(x=stadium_surface, y=`Actual difference - spread`, color=stadium_surface, fill=stadium_surface)) +
+    geom_boxplot() +
+    labs(title="Accuracy of the spread vs stadium surface played on",
+         x = "Stadium surface") +
+    theme_light() +
+    theme(legend.position="none", text = element_text(size=12)) +
+    scale_fill_manual(values = c("#ffffcc", "#a1dab4", "#41b6c4")) +
+    scale_color_manual(values = c("#a1dab4", "#41b6c4", "#2c7fb8"))
+}
+
+plot_stadium_weather <- function(dataset){
+  df <- dataset[complete.cases(dataset$stadium_weather_type), ]
+  
+  
+  ggplot(data=df, aes(x=stadium_weather_type, 
+                      y=`Actual difference - spread`, 
+                      color=stadium_weather_type, 
+                      fill=stadium_weather_type)) +
+    geom_violin() +
+    geom_boxplot(width=0.1) +
+    labs(title="Accuracy of the spread vs weather type at the stadium",
+         x = 'Stadium Weather Type') +
+    theme_light() +
+    theme(legend.position="none", text = element_text(size=12)) +
+    scale_fill_manual(values = c("#ffffcc", "#a1dab4", "#41b6c4", "#225ea8")) +
+    scale_color_manual(values = c("#a1dab4", "#41b6c4", "#2c7fb8", "#253494"))
+}
+
+plot_weather_status <- function(dataset){
+  bad_weather_days <- filter(dataset, 
+                             stadium_weather_type != 'dome' &
+                               weather_temperature <= 45 &
+                               weather_wind_mph >= 12)
+  
+  weather_status <- ifelse(dataset$stadium_weather_type != 'dome' &
+                             dataset$weather_temperature <= 45 &
+                             dataset$weather_wind_mph >= 12, 
+                           'Bad weather', 'Good weather')
+  
+  
+  data_weather_categorized <- add_column(dataset, weather_status)
+  
+  data_weather_categorized <- data_weather_categorized[complete.cases(data_weather_categorized$weather_status),]
+  
+  ggplot(data=data_weather_categorized, aes(x=weather_status, 
+                                            y=`Actual difference - spread`, 
+                                            color=weather_status, 
+                                            fill=weather_status)) +
+    geom_violin() +
+    geom_boxplot(width=0.1) +
+    labs(title="Accuracy of the spread vs weather type at the stadium",
+         x = 'Weather status') +
+    theme_light() +
+    theme(legend.position="none", text = element_text(size=12)) +
+    scale_fill_manual(values = c("#ffffcc", "#a1dab4")) +
+    scale_color_manual(values = c("#a1dab4", "#41b6c4"))
+}
